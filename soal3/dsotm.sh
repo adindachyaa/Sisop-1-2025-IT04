1920b4096e8d880880583d8cd7020206f0cb651f
Jiyel <salsabilulla77@gmail.com>
2025-03-15 17:33:51 +0700

    dsotm.sh

--- soal3/dsotm.sh
+++ soal3/dsotm.sh
@@ -16,6 +16,46 @@ for i in {1..100}; do
 done
 }
 
+ctime() {
+while true; do
+clear
+jam=$(date +"%H")
+
+if [ $jam -ge  3 ] && [ $jam -lt 4 ]; then pesan="SAHUR WOI!"
+elif [ $jam -ge 4 ] && [ $jam -lt 10 ]; then pesan="Selamat pagi, semangat beraktivitas!"
+elif [ $jam -ge 10 ] && [ $jam -lt 15 ]; then pesan="Selamat siang, lemes gak nih? Wkwk."
+elif [ $jam -ge 15 ] && [ $jam -lt 18 ]; then pesan="Selamat sore, ngabuburit ngapain? Jangan lupa nyari takjil!"
+elif [ $jam -ge 18 ] && [ $jam -lt 20 ]; then pesan="Selamat berbuka puasa, sob. Sholat teraweh jangan lupa, ya!"
+else pesan="Selamat malam, tidur sana."
+fi
+
+date +"%A, %B %d %Y - %H:%M:%S"
+echo "︶︶︶︶︶︶︶︶୨♡୧︶︶︶︶︶︶︶︶"
+echo "୨♡୧ ... $pesan"
+sleep 1
+done
+}
+
+dmoney() {
+clear
+tput civis
+trap "tput cnorm; exit" SIGINT
+
+simbol=("$" "€" "£" "¥" "¢" "₹" "₩" "₿" "₣")
+rows=$(tput lines)
+cols=$(tput cols)
+
+while true; do
+col=$((RANDOM % cols))
+for ((i=0; i<rows; i++)); do
+tput cup $i $col
+char=${simbol[$((RANDOM % ${#simbol[@]}))]}
+echo -ne "$color$char\033[95m"
+sleep 0.001
+done
+done
+}
+
 if [[ "$1" == --play=* ]]
 then
 	track="${1#*=}"	
@@ -26,7 +66,13 @@ then
 	elif [[ "$track" == "On the Run" ]]
 	then
 		ontherun
-	else
+	elif [[ "$track" == "Time" ]]
+ 	then
+  		ctime
+    	elif [[ "$track" == "Money" ]]
+     	then
+      		dmoney
+ 	else
 		echo "sorry gaada lagu itu"
 	fi
 else
