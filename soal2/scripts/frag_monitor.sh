67b4c391e7b256e304283e81b54eb7641bb4caa6
rayahmd <136787413+rayahmd@users.noreply.github.com>
2025-03-15 20:23:46 +0700

    Delete soal2 directory

--- soal2/scripts/frag_monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-memory_usage=$(free | grep Mem | awk '{printf("%.2f%", $3/$2 * 100)}')
-memory_total=$(free | grep Mem | awk '{printf ("%d MB", $2 / 1024)}')
-memory_ava=$(free | grep Mem | awk '{printf ("%d MB", $4 / 1024)}')
-
-echo "[$timestamp] - Fragment Usage [$memory_usage] - Fragment Count [Total: $memory_total, Available: $memory_ava]" 
-
